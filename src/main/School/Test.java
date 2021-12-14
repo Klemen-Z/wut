7562b66a79a80a335d7060f8d4dfe3c8eeb550da
Klemen-Z <klemenzmahar@gmail.com>
2021-12-14 09:48:22 +0100

    comit

--- src/main/School/Test.java
+++ src/main/School/Test.java
@@ -1,32 +1,53 @@
 package main.School;
 
-
 import java.util.HashMap;
 
 public class Test {
-    public static void main(String[] args) {
-        HashMap<Integer, Teacher> Teachers = new HashMap<>();
-        HashMap<Integer, Student> Students = new HashMap<>();
 
+    static HashMap<Integer, Teacher> Teachers = new HashMap<>();
+    static HashMap<Integer, Student> Students = new HashMap<>();
+
+    public static void fillT(){
         Teachers.put(1, new Teacher());
         Teachers.put(2, new Teacher("Veronica", "McCarthy", 30, "I2b"));
+    }
+    public static void fillS(){
         Students.put(1, new Student());
         Students.put(2, new Student("Anne", "Tower", 15, "I2b"));
         Students.put(3, new Student());
-        System.out.println("\n \n");
-        for(Student s: Students.values())
-        {
-            System.out.println("Student: \n");
-            System.out.println(s.getFirstName());
-            System.out.println(s.getLastName());
-            System.out.println("\n \n");
+    }
+
+    public static Teacher Teacher(int HashmapKey){
+        Teacher t = null;
+        try{
+            t = Teachers.get(HashmapKey);
         }
-        for(Teacher t: Teachers.values())
-        {
-            System.out.println("Teacher: \n");
-            System.out.println(t.getFirstName());
-            System.out.println(t.getLastName());
-            System.out.println("\n \n");
+        catch(Error e){
+            System.out.println("Object doesnt exist");
         }
+        return t;
+    }
+    public static Student Student(int HashmapKey){
+        Student s = null;
+        try{
+            s = Students.get(HashmapKey);
+        }
+        catch(Error e){
+            System.out.println("Object doesnt exist");
+        }
+        return s;
+    }
+
+    public static void main(String[] args) {
+
+        fillT();
+        fillS();
+
+        Teacher t = Teacher(1);
+        Student s = Student(1);
+
+        if (t != null) System.out.println("Teacher: \n" + t.getFirstName() + "\n ");
+        if (t != null) System.out.println("Student: \n" + s.getFirstName() + "\n ");
+
     }
 }
