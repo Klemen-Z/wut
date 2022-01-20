7d8a46d36e0d54e8640c14c450cedccde248bff3
Klemen <klemenzmahar@gmail.com>
2022-01-20 17:27:44 +0100

    Finished making the Human classes, adjusted Job to be generalised instead of abstract cause im lazy

---
+++ src/main/people/Job.java
@@ -0,0 +1,72 @@
+package main.people;
+
+public class Job {
+    int salary;
+    double mult;
+    String pos;
+    String jobName;
+    boolean fullTime;
+
+    Job(String Job_Name, int Salary, double multiplier, String Position, boolean fullTime){
+        setMult(multiplier);
+        setPos(Position);
+        setSalary(Salary);
+        setJobName(Job_Name);
+        setFullTime(fullTime);
+        getJob();
+    }
+
+    public double getMult() {
+        return mult;
+    }
+
+    public String getJobName() {
+        return jobName;
+    }
+
+    public void setFullTime(boolean fullTime) {
+        this.fullTime = fullTime;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public String getPos() {
+        return pos;
+    }
+
+    public boolean getFullTime() {
+        return this.fullTime;
+    }
+
+    public void setJobName(String jobName) {
+        this.jobName = jobName;
+    }
+
+    public void setMult(double mult) {
+        this.mult = mult;
+    }
+
+    public void setPos(String pos) {
+        this.pos = pos;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public int calcSalary(){
+        int temp;
+        temp = (int) (this.salary * this.mult);
+        return  temp;
+    }
+
+    void getJob() {
+        if (this.fullTime){
+            System.out.println("Job: " + getJobName() + ", Salary: " + calcSalary() + "Full-Time");
+        } else {
+            System.out.println("Job: " + getJobName() + ", Salary: " + calcSalary() + " Part-Time");
+        }
+    }
+}
---
+++ src/main/people/Main_Human.java
@@ -0,0 +1,8 @@
+package main.people;
+
+public class Main_Human {
+
+    public static void main(String[] args) {
+        Child Bobby = new Child(12, 130, 30, "Bob", "Kendrick", "Newspaper delivery", "Delivery boy", 5, 1, false);
+    }
+}
